1fb32a1b068a82985640385dc8e1a1f647c55499
Jucimeire Silva <jucimeiresilva@hotmail.com>
2022-02-04 16:49:44 -0300

    Fiz uma parte do NavBar porém está dando um erro que eu não consegui identificar, não consigo abrir o Rreact e ver como está ficando.

---
+++ src/components/SortingButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SortingButton = ({ children }) => {
+  return (
+    <>
+      <button>Eu sou um botão de sorting { children }</button>
+    </>
+  );
+};
+
+export default SortingButton;
