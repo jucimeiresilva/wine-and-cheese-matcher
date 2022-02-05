392f8d8c7f70df2dc80c98e20a03d6b10386160a
Jucimeire Silva <jucimeiresilva@hotmail.com>
2022-02-05 14:07:39 -0300

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React from 'react';
 
- export const NavBar = () {
-     return <nav>
-        <span> Logo </span>
-     
-     </nav>;
-
- };
+export const NavBar = () => {
+    return (
+        <nav className='navbar'>
+            <NavLink to="/" className="link">
+                <span className='logo'>Logo</span>
+            </NavLink>
+            <div className='nav-links'> 
+             <NavLink to='/wines' classname="link">
+                 Wine List
+             </NavLink>
+             <NavLink to='/favorites' className="link">
+                 Favorites
+             </NavLink>
+            </div>  
+         </nav>
+    );
+};
