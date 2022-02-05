efd5df98446bea002a942926de9fe9b5bb634bff
Jucimeire Silva <jucimeiresilva@hotmail.com>
2022-02-05 15:38:02 -0300

    marcação da home feita

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 import WineList from './components/WineList';
 import {NavBar} from "./components/Navbar";
 import {Routes, Route} from 'react-router-dom'
+import Home from './components/Home';
 
 function App() {
   return (
       <div className="App">
         <NavBar/>
         <Routes>
+          <Route path='/'  element={<Home/>} />
           <Route path="/wines" element={<WineList />} />
         </Routes>
+
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <>
+        <div className="container" style={{border:"solid black 2px"}}>
+            <h1>Olá</h1>
+            <button>Oi</button>
+        </div>
+        </>
+    );
+};
+
+
+export default Home;
