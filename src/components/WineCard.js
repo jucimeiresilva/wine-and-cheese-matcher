a1b28de15c4eb1b39465be9106720d87b1cd146e
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-02-08 20:05:59 +0100

    estilização wineList e wineCard

--- src/components/WineCard.js
+++ src/components/WineCard.js
@@ -6,7 +6,7 @@ import "./WineList.css";
 const WineCard = ({ Name, Winery, Country, Type, vintage, rating }) => {
   return (
     <>
-      <div className="card mb-3 col">
+      <div className="card mb-3 col wine-hover" >
         <div className="row g-0">
           <div className="col-3">
             <img
@@ -16,7 +16,7 @@ const WineCard = ({ Name, Winery, Country, Type, vintage, rating }) => {
               style={{ width: "80px" }}
             />
           </div>
-          <div className="col-md">
+          <div className="col-md d-flex flex-column" style={{padding:"24px 16px 24px 0px"}}>
             <div className="card-body ms-2">
               <div
                 className="d-flex align-items-start justify-content-between"
@@ -25,6 +25,33 @@ const WineCard = ({ Name, Winery, Country, Type, vintage, rating }) => {
                 <div className="me-5">
                   <img src={starOn} alt="Favorited" className="favorite-star" />
                 </div>
+              </div>
+              <div className="card-details d-flex">
+                <div className="d-flex flex-column col me-1 card-column justify-content-between">
+                  <div>
+                    <span className="subtitle me-1">Winery</span>
+                    <span className="body2">{Winery}</span>
+                  </div>
+                  <div>
+                    <span className="subtitle me-1">Type</span>
+                    <span className="body2">{Type}</span>
+                  </div>
+                </div>
+                <div className="d-flex flex-column col me-1 card-column justify-content-between">
+                  <div>
+                    <span className="subtitle me-1">Country</span>
+                    <span className="body2">{Country}</span>
+                  </div>
+                  <div>
+                    <span className="subtitle me-1">Year</span>
+                    <span className="body2">{vintage}</span>
+                  </div>
+                </div>
+                <div className="d-flex flex-column col card-column ">
+                    <span className="me-1 matching">{rating.toFixed(1)} </span>
+                    <span className="body2">Rating</span>
+                </div>
+                
               </div>
           
               </div>
