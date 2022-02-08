cb4e133ef719629883afa2d05f1355c3f61306e4
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-02-08 19:22:01 +0100

    Limpei o WineCard pra ficar mais fácil de reorganizar

--- src/components/WineCard.js
+++ src/components/WineCard.js
@@ -1,7 +1,7 @@
 import React from "react";
-import starOn from '../assets/Property 1=on.png'
-import starOff from '../assets/Property 1=off.png'
-import './WineList.css'
+import starOn from "../assets/Property 1=on.png";
+import starOff from "../assets/Property 1=off.png";
+import "./WineList.css";
 
 const WineCard = ({ Name, Winery, Country, Type, vintage, rating }) => {
   return (
@@ -13,39 +13,24 @@ const WineCard = ({ Name, Winery, Country, Type, vintage, rating }) => {
               src="https://shop.foleyfoodandwinesociety.com/assets/images/products/pictures/8174-560.png"
               className="img-fluid ms-5 me-1 "
               alt={`${Name} bottle`}
-              style={{ width: "80px"}}
+              style={{ width: "80px" }}
             />
           </div>
           <div className="col-md">
             <div className="card-body ms-2">
-              <div className="d-flex align-items-start justify-content-between" style={{width:"647px"}}>
-                <h3 className="card-title mb-4"> { Name } </h3>
-                <div>
-                  <img src={starOn} alt='Favorited' className="favorite-star" />
+              <div
+                className="d-flex align-items-start justify-content-between"
+              >
+                <h3 className="card-title mb-4"> {Name} </h3>
+                <div className="me-5">
+                  <img src={starOn} alt="Favorited" className="favorite-star" />
                 </div>
               </div>
-              <div className="row">
-                <div className="card-text col">
-                  <span className="subtitle">
-                    Winery: 
-                  </span>
-                  <span className="body2">
-                    {Winery} 
-                  </span>
-                </div>
-                <div className="card-text col">Country: {Country} </div>
-              </div>   
-              <div className="row">
-                <div className="card-text col">Type: {Type} </div>
-                <div className="card-text col">Year: {vintage} </div>
-              </div>
-              <div className="row">
-                <div className="card-text col">Rating: {rating.toFixed(1)} </div>
+          
               </div>
             </div>
           </div>
         </div>
-      </div>
     </>
   );
 };
