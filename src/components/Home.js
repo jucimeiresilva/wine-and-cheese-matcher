8ab29d07f787b001a04cb5493092a7390cc6e23c
Jucimeire Silva <jucimeiresilva@hotmail.com>
2022-02-16 00:18:01 -0300

    botão home feito

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,20 @@
 import React from "react";
 
 const Home = () => {
-    return(
-        <>
-        <div className="container" style={{border:"solid black 2px"}}>
-            <h1>Olá</h1>
-            <button>Oi</button>
-        </div>
-        </>
-    );
-};
+  return (
+    <>
+      <div className="container" style={{ border: "solid black 2px" }}>
+        <p> WELCOME, </p>
+        <p> FIND THE PERFECT </p>
+        <h3> WINE AND CHEESE </h3>
+        <p> MATCH! </p>
 
+        <button>
+          <a> SEE WINE LIST</a>
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Home;
