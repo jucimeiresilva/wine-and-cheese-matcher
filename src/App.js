acb9b402cf224b61de58c3fce4d4f204b124c380
Jucimeire Silva <jucimeiresilva@hotmail.com>
2022-02-17 15:48:11 -0300

    about em andamento

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'normalize.css'
 import 'bootstrap/dist/css/bootstrap.css';
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Link} from 'react-router-dom'
 import WineList from './components/WineList';
 import NavBar from "./components/Navbar";
 import Home from './components/Home';
@@ -8,6 +8,7 @@ import './App.css'
 import WineName from './components/WineName';
 import Favorites from './components/Favorites';
 import { Button } from 'bootstrap';
+import About from './components/About';
 
 
 function App() {
@@ -18,10 +19,13 @@ function App() {
           <Route path='/'  element={<Home/>} />
           <Route path="/wines" element={<WineList />} />
           <Route path="/wine-details" element={<WineName/>}/>
-          <Route path="/favorites" element={<Favorites />}/>
+          <Route path="/favorites" element={<Favorites/>}/>
+          <Route path="/home" element={<About/>}/>
         </Routes>
 
-    </div>
+      </div>
+
+  
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+import NavBar from "./Navbar";
+
+const About = () => {
+    return(
+        <>
+        <div className="container" style={{border:"solid black 2px"}}>
+            <h1>ABOUT</h1>
+            <h3>SHORT DESCRIPTION ABOUT THE PROJECT</h3>
+        </div>
+
+        <footer>2022</footer>
+        </>
+    );
+};
+
+
+
+
+export default About;
