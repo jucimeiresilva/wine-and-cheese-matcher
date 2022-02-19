a789e036ac5d4e161dfd07592173b24f883f33fb
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-02-19 14:50:18 +0100

    wineDetails funcional

--- src/components/CheeseCard.js
+++ src/components/CheeseCard.js
@@ -3,10 +3,10 @@ import React from "react";
 const CheeseCard = ({cheeseName}) => {
     return (
        
-             <div class="card" style={{height: '300px'}}>
-             <img src="./image/cheese.png" class="card-img-top" alt="..."/>
-         <div class="card-body">
-            <h5 class="card-title">{cheeseName}</h5>
+             <div className="card" style={{height: '300px'}}>
+             <img src="./image/cheese.png" className="card-img-top" alt="..."/>
+         <div className="card-body">
+            <h5 className="card-title">{cheeseName}</h5>
         </div>
         </div>
     
--- src/components/WineName.js
+++ src/components/WineName.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import CheeseCard from "./CheeseCard";
 
 
@@ -22,64 +23,72 @@ const typesCheese = [
   "coalho",
 ];
 
-const WineName = () => {
+const WineName = ({wineList}) => {
+  
+
+  const { id } = useParams()
+  const getWine = () => {
+    return wineList.filter((wine) => wine._id === id) 
+  }
+  
   const [cheese, setCheese] = useState(typesCheese);
+  const [wine, setWine] = useState(getWine())
 
   return (
-    <div
-      className="container-fluid px-5"
-      style={{
-        backgroundColor: "antiquewhite",
-        maxWidth: "1600px",
-        width: "90vw",
-        minHeight: "100vh",
-      }}
-    >
       <div
         className="container-fluid px-5"
-        style={{ border: "solid black 1px" }}
+        style={{
+          backgroundColor: "antiquewhite",
+          maxWidth: "1600px",
+          width: "90vw",
+          minHeight: "100vh",
+        }}
       >
-        <h1>Wine Details</h1>
-        <div className="container d-flex">
-          <div className="card" style={{ width: "18rem" }}>
-            <img
-              src="https://www.crystalimagesinc.com/wp-content/uploads/wine_cake_heart05-1.jpg"
-              className="card-img-top"
-              alt="..."
-            />
-            <div className="card-body">
-              <div>
-                <h3>Weinkellerei Am Klostergarten Wollsteiner Rheingrafenstein Spatlese Kerner 2016</h3>
+        <div
+          className="container-fluid px-5"
+          style={{ border: "solid black 1px" }}
+        >
+          <h1>Wine Details</h1>
+          <div className="container d-flex">
+            <div className="card" style={{ width: "18rem" }}>
+              <img
+                src="https://www.crystalimagesinc.com/wp-content/uploads/wine_cake_heart05-1.jpg"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
                 <div>
-                  <div className="d-flex align-items-center">
-                    <p className="subtitle me-2">Winery</p>
-                    <p className="body2">Chalk Hill</p>
-                  </div>
-                  <div className="d-flex align-items-center">
-                    <p className="subtitle me-2">Type</p>
-                    <p className="body2">Red</p>
-                  </div>
-                  <div className="d-flex align-items-center">
-                    <p className="subtitle me-2">Country</p>
-                    <p className="body2">United States</p>
-                  </div>
-                  <div className="d-flex align-items-center">
-                    <p className="subtitle me-2">Year</p>
-                    <p className="body2">2018</p>
+                  <h3>{wine[0].Name}</h3>
+                  <div>
+                    <div className="d-flex align-items-center">
+                      <p className="subtitle me-2">Winery</p>
+                      <p className="body2">{wine[0].Winery}</p>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <p className="subtitle me-2">Type</p>
+                      <p className="body2">{wine[0].Type}</p>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <p className="subtitle me-2">Country</p>
+                      <p className="body2">{wine[0].Country}</p>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <p className="subtitle me-2">Year</p>
+                      <p className="body2">{wine[0].vintage}</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div>
-          <h3> 10 MATCHING CHEESE</h3>
-          <div class="card-group">
-          <CheeseCard cheeseName='Parmesão'/>
-          </div>
+            <div>
+            <h3> 10 MATCHING CHEESE</h3>
+            <div className="card-group">
+            <CheeseCard cheeseName='Parmesão'/>
+            </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
