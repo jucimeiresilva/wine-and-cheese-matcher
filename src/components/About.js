24c7c0e489c40e1e3ed42f6e5b81e6b61ddb7ec0
Jucimeire Silva <jucimeiresilva@hotmail.com>
2022-02-19 11:00:49 -0300

    cardAbout

--- src/components/About.js
+++ src/components/About.js
@@ -10,6 +10,9 @@ const About = () => {
         <div className="container" style={{border:"solid black 2px"}}>
             <h1>ABOUT</h1>
             <h3>SHORT DESCRIPTION ABOUT THE PROJECT</h3>
+            <CardAbout/>
+                
+            
         </div>
         </>
 
