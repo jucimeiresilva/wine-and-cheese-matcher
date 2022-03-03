f8666c11a7319e7b3a1764cba8503d8986f53e48
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-03-03 02:22:01 +0100

    cheeseCard atualisado

--- src/components/CheeseCard.js
+++ src/components/CheeseCard.js
@@ -1,21 +1,19 @@
 import React from "react";
+import "./CheeseCard.css";
 
 const CheeseCard = ({ cheeseName, img }) => {
   return (
-    <div className="card" style={{ height: "250px", textAlign: "center", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.08)" }}>
+    <div className="card cheese-card">
       <div className="card-body d-flex flex-column justify-content-end">
         <div>
           <img
             src={img}
-            style={{ maxHeight: "180px", maxWidth: "200px" }}
             className="card-img-top mx-auto my-auto cheese-img"
             alt={cheeseName}
           />
         </div>
         <div>
-          <h5 className="card-title subtitle mt-2">
-            {cheeseName}
-          </h5>
+          <h5 className="card-title subtitle mt-2">{cheeseName}</h5>
         </div>
       </div>
     </div>
